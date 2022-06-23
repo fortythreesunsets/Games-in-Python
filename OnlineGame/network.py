8f4e51bdaf7adba2042f930baca785e4c9b0fbb4
fortythreesunsets <sruv_2601@hotmail.com>
2022-06-23 13:49:01 -0500

    Update network.py

--- OnlineGame/network.py
+++ OnlineGame/network.py
@@ -7,7 +7,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.0.10"
+        self.server = "192.168.0.00" #IP address (change)
         self.port = 5555
         self.addr = (self.server, self.port)
         self.player = self.connect()
